5e5d991c30988eedbfee7aab828d92bbebd5a8cb
sahadat-11 <137852614+sahadat-11@users.noreply.github.com>
2023-11-25 18:29:33 +0600

    added

--- Inheritence/src/Inheritence/Inheritence_with_Constructor.java
+++ Inheritence/src/Inheritence/Inheritence_with_Constructor.java
@@ -36,3 +36,136 @@ public class Inheritence_with_Constructor {
       //  Base2 b2 = new Base2(3);
     }
 }
+
+
+// Java Program to Illustrate
+// Invocation of Constructor
+// Calling Without Usage of super Keyword
+// Super class
+class Base {
+
+    // Constructor of super class
+    Base()
+    {
+        // Print statement
+        System.out.println(
+                "Base Class Constructor Called ");
+    }
+}
+
+// Class 2
+// Sub class
+class Derived extends Base {
+
+    // Constructor of sub class
+    Derived()
+    {
+
+        // Print statement
+        System.out.println(
+                "Derived Class Constructor Called ");
+    }
+}
+
+// Class 3
+// Main class
+class GFG {
+
+    // Main driver method
+    public static void main(String[] args)
+    {
+
+        // Creating an object of sub class
+        // inside main() method
+        Derived d = new Derived();
+
+        // Note: Here first super class constructor will be
+        // called there after derived(sub class) constructor
+        // will be called
+    }
+}
+//Base Class Constructor Called
+//Derived Class Constructor Called
+
+
+
+
+// Java program to illustrate the
+// concept of inheritance
+// base class
+class Bicycle {
+    // the Bicycle class has two fields
+    public int gear;
+    public int speed;
+
+    // the Bicycle class has one constructor
+    public Bicycle(int gear, int speed)
+    {
+        this.gear = gear;
+        this.speed = speed;
+    }
+
+    // the Bicycle class has three methods
+    public void applyBrake(int decrement)
+    {
+        speed -= decrement;
+    }
+
+    public void speedUp(int increment)
+    {
+        speed += increment;
+    }
+
+    // toString() method to print info of Bicycle
+    public String toString()
+    {
+        return ("No of gears are " + gear + "\n"
+                + "speed of bicycle is " + speed);
+    }
+}
+
+// derived class
+class MountainBike extends Bicycle {
+
+    // the MountainBike subclass adds one more field
+    public int seatHeight;
+
+    // the MountainBike subclass has one constructor
+    public MountainBike(int gear, int speed,
+                        int startHeight)
+    {
+        // invoking base-class(Bicycle) constructor
+        super(gear, speed);
+        seatHeight = startHeight;
+    }
+
+    // the MountainBike subclass adds one more method
+    public void setHeight(int newValue)
+    {
+        seatHeight = newValue;
+    }
+
+    // overriding toString() method
+    // of Bicycle to print more info
+    @Override public String toString()
+    {
+        return (super.toString() + "\nseat height is "
+                + seatHeight);
+    }
+}
+
+// driver class
+public class Test {
+    public static void main(String args[])
+    {
+
+        MountainBike mb = new MountainBike(3, 100, 25);
+        System.out.println(mb.toString());
+    }
+}
+//No of gears are 3
+//speed of bicycle is 100
+//seat height is 25
+
+
+
